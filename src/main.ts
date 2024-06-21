a7af1db9279e0390fef3e1409ede192c6617defe
Logon92 <you@example.com>
2024-06-21 12:36:37 +0100

    guardar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-function areaDelCuadrado (lado : number) : number {
-    return lado * lado;
-}
+import {InfoCarta, cartas} from "./modelo";
 
-console.log(areaDelCuadrado(10));
+const bucleCartas = (cartas:InfoCarta[]) => {
+    for (let i = 0; i < cartas.length; i++){
+        const dataIndiceId = `[data-indice-id="${i}"]`;
+        const elementoDiv = document.querySelector(`div${dataIndiceId}`);
+        if(elementoDiv instanceof HTMLDivElement && elementoDiv !== null && elementoDiv !== undefined)(
+            elementoDiv.addEventListener("click", function() {
+                const elementoImg = document.querySelector(`img${dataIndiceId}`)
+                if(elementoImg instanceof HTMLImageElement && elementoImg !== null && elementoImg !== undefined){
+                    elementoImg.src = cartas[i].imagen
+                }
+            })
+        );
+    }
+};
+console.log(bucleCartas(cartas));
+
+cartas.sort(() => 0.5 - Math.random()); // Ordenar de forma aleatoria
---
+++ src/modelo.ts
@@ -0,0 +1,68 @@
+export interface InfoCarta {
+    idFoto: number;
+    nombre: string;
+    imagen: string;
+};
+
+export const cartas: InfoCarta[] = [
+    {
+        idFoto: 1,
+        nombre: "Grogu",
+        imagen: "https://eltallerdehector.com/wp-content/uploads/2022/06/335ff-baby-yoda-png.png",
+    },
+    {
+        idFoto: 2,
+        nombre: "Grogu",
+        imagen: "https://eltallerdehector.com/wp-content/uploads/2022/06/335ff-baby-yoda-png.png",
+    },
+    {
+        idFoto: 3,
+        nombre: "Darth Vader",
+        imagen: "https://png.pngtree.com/png-vector/20230801/ourmid/pngtree-vader-clipart-darth-vader-cartoon-by-big-emu-shop-vector-png-image_6828070.png",
+    },
+    {
+        idFoto: 4,
+        nombre: "Darth Vader",
+        imagen: "https://png.pngtree.com/png-vector/20230801/ourmid/pngtree-vader-clipart-darth-vader-cartoon-by-big-emu-shop-vector-png-image_6828070.png",
+    },
+    {
+        idFoto: 5,
+        nombre: "Chewbacca",
+        imagen: "https://png.pngtree.com/png-vector/20230729/ourmid/pngtree-chewbacca-clipart-chewbacca-character-vector-for-use-cartoon-png-image_6793961.png",
+    },
+    {
+        idFoto: 6,
+        nombre: "Chewbacca",
+        imagen: "https://png.pngtree.com/png-vector/20230729/ourmid/pngtree-chewbacca-clipart-chewbacca-character-vector-for-use-cartoon-png-image_6793961.png",
+    },
+    {
+        idFoto: 7,
+        nombre: "Mandalorian",
+        imagen: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4560c1bc-691a-41d7-b7e8-00db8ef5198a/dfcjt40-8ce0d474-e667-4e80-8735-f3bd2cb5a06e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ1NjBjMWJjLTY5MWEtNDFkNy1iN2U4LTAwZGI4ZWY1MTk4YVwvZGZjanQ0MC04Y2UwZDQ3NC1lNjY3LTRlODAtODczNS1mM2JkMmNiNWEwNmUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.FrMxKEvrhBC810f1UlaYlYcxn0PFWtnEySwyBdKRvbc",
+    },
+    {
+        idFoto: 8,
+        nombre: "Mandalorian",
+        imagen: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4560c1bc-691a-41d7-b7e8-00db8ef5198a/dfcjt40-8ce0d474-e667-4e80-8735-f3bd2cb5a06e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ1NjBjMWJjLTY5MWEtNDFkNy1iN2U4LTAwZGI4ZWY1MTk4YVwvZGZjanQ0MC04Y2UwZDQ3NC1lNjY3LTRlODAtODczNS1mM2JkMmNiNWEwNmUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.FrMxKEvrhBC810f1UlaYlYcxn0PFWtnEySwyBdKRvbc",
+    },
+    {
+        idFoto: 9,
+        nombre: "C3PO",
+        imagen: "https://png.pngtree.com/png-vector/20230822/ourmid/pngtree-star-wars-character-line-icon-on-white-background-vector-png-image_6843228.png",
+    },
+    {
+        idFoto: 10,
+        nombre: "C3PO",
+        imagen: "https://png.pngtree.com/png-vector/20230822/ourmid/pngtree-star-wars-character-line-icon-on-white-background-vector-png-image_6843228.png",
+    },
+    {
+        idFoto: 11,
+        nombre: "Soldado imperial",
+        imagen: "https://png.pngtree.com/png-vector/20230531/ourmid/pngtree-star-wars-trooper-stickers-free-vector-clipart-illustration-png-image_6780799.png",
+    },
+    {
+        idFoto: 12,
+        nombre: "Soldado imperial",
+        imagen: "https://png.pngtree.com/png-vector/20230531/ourmid/pngtree-star-wars-trooper-stickers-free-vector-clipart-illustration-png-image_6780799.png",
+    },
+];
